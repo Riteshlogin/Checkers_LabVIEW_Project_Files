886ea2b071f55dc754f0285ee06d45e64934a7c1
Ritesh Misra <riteshlogin@gmail.com>
2016-12-04 12:28:51 -0500

    Added a little bit more
    Have yet to test it

--- Logic.c
+++ Logic.c
@@ -26,12 +26,36 @@ if(piece == 1){
 		moves[0][1] = coordinates[1] - 1;
 		
 	}
-	
+	//Illegal move because friend in way
 	else if(coordinates[1] == 7 && coordinates[0] < 7 && input[coordinates[0]+1][coordinates[1]-1] == 1)
 	{      //row,col
 		moves[0][0] = -1;
 		moves[0][1] = -1;
 		
+	}
+	//Illegal move because friend in way
+	else if(coordinates[1] == 7 && coordinates[0] < 7 && input[coordinates[0]+1][coordinates[1]-1] == 1)
+	{      //row,col
+		moves[0][0] = -1;
+		moves[0][1] = -1;
+	}
+	
+	else if(coordinates[1] == 7 && coordinates[0] < 6 && input[coordinates[0]+1][coordinates[1]-1] == 2 && input[coordinates[0]+2][coordinates[1]-2] == 0)
+	{
+		moves[0][0] = coordinates[0]+2;
+		moves[0][1] = coordinates[0]-2;	
+	}
+	
+	else if(coordinates[1] == 0 && coordinates[0] < 6 && input[coordinates[0]+1][coordinates[1]+1] == 2 && input[coordinates[0]+2][coordinates[1]+2] == 0)
+	{
+		moves[0][0] = coordinates[0]+2;   
+		moves[0][1] = coordinates[0]+2;	
+	}
+	
+	else if(input[coordinates[0]+1][coordinates[1]+1] == 2 && input[coordinates[0]+2][coordinates[1]+2] == 0)
+	{
+		moves[0][0] = coordinates[0]+2;   
+		moves[0][1] = coordinates[0]+2;	
 	}
 	
 }
