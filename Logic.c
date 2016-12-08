55d93834b77a7d8e597527f827714ac14994bbc5
Ritesh Misra <riteshlogin@gmail.com>
2016-12-08 10:16:18 -0500

    Logic restructuring

--- Logic.c
+++ Logic.c
@@ -4,59 +4,120 @@ int32 testy;
 
 
 int32 piece = input[coordinates[0]][coordinates[1]];
-
+int secondMove=0;
 if(piece == 1){
-
-    if(coordinates[1] == 0 && coordinates[0] < 7 && input[coordinates[0]+1][coordinates[1]+1] == 0)
-	{
-		moves[0][0] = coordinates[0] + 1;
-		moves[0][1] = coordinates[1] + 1;		
-    }
 	
-	if(coordinates[1] == 0 && coordinates[0] < 7 && input[coordinates[0]+1][coordinates[1]+1] == 1)
+	//For moving left
+	if(coordinates[1] != 0)
 	{
-		moves[0][0] = -1;
-		moves[0][1] = -1;
+		if(input[coordinates[0]+1][coordinates[1]-1] != 1)
+		{
+			if(input[coordinates[0]+1][coordinates[1]-1] == 0)
+			{	//Nothing in the way 
+				moves[secondMove][0] = coordinates[0] + 1;
+				moves[secondMove][1] = coordinates[1] - 1;
 				
-    }
+				secondMove = 1;
+			}
+			
+			if(coordinates[1] > 1)
+			{
+				if(input[coordinates[0]+1][coordinates[1]-1] == 2 && input[coordinates[0]+2][coordinates[1]-2] == 0)
+				{
+					moves[secondMove][0] = coordinates[0] + 2;
+					moves[secondMove][1] = coordinates[1] - 2;
+					secondMove = 1;					
+				}
+			
+			}
 	
-	else if(coordinates[1] == 7 && coordinates[0] < 7 && input[coordinates[0]+1][coordinates[1]-1] == 0)
-	{      //row,col
-		moves[0][0] = coordinates[0] + 1;
-		moves[0][1] = coordinates[1] - 1;
+		}
 		
 	}
-	//Illegal move because friend in way
-	else if(coordinates[1] == 7 && coordinates[0] < 7 && input[coordinates[0]+1][coordinates[1]-1] == 1)
-	{      //row,col
-		moves[0][0] = -1;
-		moves[0][1] = -1;
+	//For moving right
+	if(coordinates[1] != 6)
+	{
+		if(input[coordinates[0]+1][coordinates[1]+1] != 1)
+		{
+			if(input[coordinates[0]+1][coordinates[1]+1] == 0)
+			{	//Nothing in the way 
+				moves[secondMove][0] = coordinates[0] + 1;
+				moves[secondMove][1] = coordinates[1] + 1;
+			}
+			
+			if(coordinates[1] < 6)
+			{
+				if(input[coordinates[0]+1][coordinates[1]+1] == 2 && input[coordinates[0]+2][coordinates[1]+2] == 0)
+				{
+					moves[secondMove][0] = coordinates[0] + 2;
+					moves[secondMove][1] = coordinates[1] + 2;	
+				}
+				
+			}
+	
+		}
 		
 	}
-	//Illegal move because friend in way
-	else if(coordinates[1] == 7 && coordinates[0] < 7 && input[coordinates[0]+1][coordinates[1]-1] == 1)
-	{      //row,col
-		moves[0][0] = -1;
-		moves[0][1] = -1;
+	
+}
+
+if(piece == 2)
+{
+	
+	if(coordinates[1] != 0)
+	{
+		if(input[coordinates[0]-1][coordinates[1]-1] != 2)
+		{
+			if(input[coordinates[0]-1][coordinates[1]-1] == 0)
+			{	//Nothing in the way 
+				moves[secondMove][0] = coordinates[0] - 1;
+				moves[secondMove][1] = coordinates[1] - 1;
+				
+				secondMove = 1;
+			}
+			
+			if(coordinates[1] > 1)
+			{
+				if(input[coordinates[0]-1][coordinates[1]-1] == 1 && input[coordinates[0]-2][coordinates[1]-2] == 0)
+				{
+					moves[secondMove][0] = coordinates[0] - 2;
+					moves[secondMove][1] = coordinates[1] - 2;
+					secondMove = 1;					
+				}
+			
+			}
+	
+		}
+		
+	}
+	//For moving right
+	if(coordinates[1] != 6)
+	{
+		if(input[coordinates[0]-1][coordinates[1]+1] != 1)
+		{
+			if(input[coordinates[0]-1][coordinates[1]+1] == 0)
+			{	//Nothing in the way 
+				moves[secondMove][0] = coordinates[0] + 1;
+				moves[secondMove][1] = coordinates[1] + 1;
+			}
+			
+			if(coordinates[1] < 6)
+			{
+				if(input[coordinates[0]-1][coordinates[1]+1] == 1 && input[coordinates[0]-2][coordinates[1]+2] == 0)
+				{
+					moves[secondMove][0] = coordinates[0] - 2;
+					moves[secondMove][1] = coordinates[1] + 2;	
+				}
+				
+			}
+	
+		}
+		
 	}
 	
-	else if(coordinates[1] == 7 && coordinates[0] < 6 && input[coordinates[0]+1][coordinates[1]-1] == 2 && input[coordinates[0]+2][coordinates[1]-2] == 0)
-	{
-		moves[0][0] = coordinates[0]+2;
-		moves[0][1] = coordinates[0]-2;	
-	}
 	
-	else if(coordinates[1] == 0 && coordinates[0] < 6 && input[coordinates[0]+1][coordinates[1]+1] == 2 && input[coordinates[0]+2][coordinates[1]+2] == 0)
-	{
-		moves[0][0] = coordinates[0]+2;   
-		moves[0][1] = coordinates[0]+2;	
-	}
 	
-	else if(input[coordinates[0]+1][coordinates[1]+1] == 2 && input[coordinates[0]+2][coordinates[1]+2] == 0)
-	{
-		moves[0][0] = coordinates[0]+2;   
-		moves[0][1] = coordinates[0]+2;	
-	}
+	
 	
 }
 	
@@ -67,5 +128,3 @@ if(piece == 1){
 	
 	
 
-
-}
